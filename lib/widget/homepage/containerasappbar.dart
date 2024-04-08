c45845d9771e6016f3596ee3437e98c2ff7d8ad9
mohamedamr77 <moamr947@gmail.com>
2024-04-08 17:22:03 +0200

    add ixon launcher

--- lib/widget/homepage/containerasappbar.dart
+++ lib/widget/homepage/containerasappbar.dart
@@ -8,24 +8,26 @@ import '../../core/fontcore.dart';
 class CotainerAsAppBar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: 375,
-      height: 125,
-      decoration: const BoxDecoration(
-          color: ColorStatic.primaryColor,
-          borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(30),
-            bottomLeft: Radius.circular(30),
-          )
-      ),
-      child: const Center(
-        child: Text(TextStatic.appBarText,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontFamily: FontFamily.almaraiFont,
-            color: ColorStatic.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
+    return  SafeArea(
+      child: Container(
+        width: 375,
+        height: 125,
+        decoration: const BoxDecoration(
+            color: ColorStatic.primaryColor,
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30),
+              bottomLeft: Radius.circular(30),
+            )
+        ),
+        child: const Center(
+          child: Text(TextStatic.appBarText,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: FontFamily.almaraiFont,
+              color: ColorStatic.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
           ),
         ),
       ),
